2003143b7b80eac19acdfe10a07c6c6a496213df
aaric <vipaaric@gmail.com>
2018-12-07 16:51:50 +0800

    0.0.1-SNAPSHOT 初始化

---
+++ src/main/java/com/github/aaric/achieve/obs/Application.java
@@ -0,0 +1,21 @@
+package com.github.aaric.achieve.obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Launcher.
+ *
+ * @author Aaric, created on 2018-12-07T16:46.
+ * @since 0.0.1-SNAPSHOT
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Main
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/github/aaric/achieve/obs/ObsTest.java
@@ -0,0 +1,18 @@
+package com.github.aaric.achieve.obs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * ObsTest
+ *
+ * @author Aaric, created on 2018-12-07T16:49.
+ * @since 0.0.1-SNAPSHOT
+ */
+public class ObsTest {
+
+    @Test
+    public void testHello() {
+        Assert.assertNotNull("hello obs");
+    }
+}
